ff7d4bb3b8bd0e1c58cadbfe11f2b8395a893ecc
VitorORalves <voractl@gmail.com>
2023-06-06 22:44:28 -0300

    implementing bishop

--- src/chess/piece/Pawn.java
+++ src/chess/piece/Pawn.java
@@ -11,11 +11,6 @@ public class Pawn extends ChessPiece {
 		super(board, color);
 	}
 
-	private boolean canMove(Position position) {
-		ChessPiece p = (ChessPiece) getBoard().piece(position);
-		return p == null || p.getColor() != getColor();
-	}
-
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
